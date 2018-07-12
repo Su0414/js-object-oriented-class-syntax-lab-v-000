c06ab1c0055f0be03a1a0fbc8c2cba75116602f3
Suchitra Naik <susanaik@gmail.com>
2018-07-12 18:50:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ constructor(name, homeState, training){
 
 class Ceo extends BoardMember{
   constructor(name, homeState, training){
-    super(name, homeState, training)    
+    super(name, homeState, training)
   }
-
+  hireEmployee = function () {
+    return 'Welcome aboard!';
+  }
 }
